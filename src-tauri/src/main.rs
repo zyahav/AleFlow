f9fb7e4f1e45742b6fb1fd24c15b433ac15d7f39
CJ Pais <cj@cjpais.com>
2025-12-10 22:01:04 -0800

    Set WEBKIT_DISABLE_DMABUF_RENDERER=1 in env (#427)
    * default WEBKIT_DISABLE_DMABUF_RENDERER disable
    
    * try even earlier?

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(target_os = "linux")]
+    {
+        if std::path::Path::new("/dev/dri").exists()
+            && std::env::var("WAYLAND_DISPLAY").is_err()
+            && std::env::var("XDG_SESSION_TYPE").unwrap_or_default() == "x11"
+        {
+            std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+        }
+    }
+
     handy_app_lib::run()
 }
