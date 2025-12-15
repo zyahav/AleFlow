157a7ab5a0e613d64da64d0249a0ec7699d04cd0
Rayearth <15678702+Rejerf@users.noreply.github.com>
2025-12-15 04:59:07 +0100

    Add Polish translation (#453)
    * Polish language added to language list
    
    * Polish translation added
    
    * Corrected language code for Polish

--- src/i18n/languages.ts
+++ src/i18n/languages.ts
@@ -17,4 +17,5 @@ export const LANGUAGE_METADATA: Record<
   de: { name: "German", nativeName: "Deutsch", priority: 5 },
   ja: { name: "Japanese", nativeName: "日本語", priority: 6 },
   vi: { name: "Vietnamese", nativeName: "Tiếng Việt", priority: 7 },
+  pl: { name: "Polish", nativeName: "Polski", priority: 8 },
 };
